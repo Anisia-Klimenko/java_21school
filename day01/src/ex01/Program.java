ea622f37a3ef4d6d1f338f352a815075b3333dc2
Attack Cristina <acristin@at-g6.msk.21-school.ru>
2022-06-20 11:10:30 +0300

    day01/ex01

---
+++ day01/src/ex01/Program.java
@@ -0,0 +1,13 @@
+package ex01;
+
+public class Program {
+    public static void main(String[] args) {
+        User usr1 = new User();
+        System.out.println("User1 id: " + usr1.getId());
+        User usr2 = new User();
+        System.out.println("User1 id: " + usr2.getId());
+        User usr3 = new User();
+        System.out.println("User1 id: " + usr3.getId());
+
+    }
+}
---
+++ day01/src/ex01/UserIdsGenerator.java
@@ -0,0 +1,14 @@
+package ex01;
+
+public class UserIdsGenerator {
+    private static int id = 0;
+    private static final UserIdsGenerator UIGen = new UserIdsGenerator();
+
+    public static UserIdsGenerator getInstance() {
+        return UIGen;
+    }
+
+    public static int generateId() {
+        return id++;
+    }
+}
