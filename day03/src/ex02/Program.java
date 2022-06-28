fcc6ac99a34b5160bcfe189d65ca2dbd53f02643
Anisia-Klimenko <klimenko.an11@gmail.com>
2022-06-28 15:28:45 +0300

    day03/ex03 -> start

--- day03/src/ex02/Program.java
+++ day03/src/ex02/Program.java
@@ -8,7 +8,7 @@ import static java.lang.Math.random;
 
 public class Program {
     public static String getArgs(String string) {
-        if (!string.startsWith("--arraySize=") && !string.startsWith("--threadCount=")) {
+        if (!string.startsWith("--arraySize=") && !string.startsWith("--threadsCount=")) {
             return null;
         }
 
@@ -28,16 +28,16 @@ public class Program {
 
     public static void main(String[] args) {
         if (args.length != 2) {
-            System.err.println("Should be 2 arguments: --arraySize=_ --threadCount=_");
+            System.err.println("Should be 2 arguments: --arraySize=_ --threadsCount=_");
             System.exit(-1);
         }
 
         int arraySize = 0;
-        int threadCount = 0;
+        int threadsCount = 0;
 
         try {
             arraySize = Integer.parseInt(Objects.requireNonNull(getArgs(args[0].startsWith("--arraySize=") ? args[0] : args[1])));
-            threadCount = Integer.parseInt(Objects.requireNonNull(getArgs(args[0].startsWith("--arraySize=") ? args[1] : args[0])));
+            threadsCount = Integer.parseInt(Objects.requireNonNull(getArgs(args[0].startsWith("--arraySize=") ? args[1] : args[0])));
         } catch (NullPointerException e) {
             System.err.println("Wrong arguments");
             System.exit(-1);
@@ -52,12 +52,12 @@ public class Program {
 
         System.out.println("Sum: " + findSum(array));
 
-        Integer sizeOfThread = arraySize / threadCount;
+        int sizeOfThread = arraySize / threadsCount;
 
-        List<Thread> threadList = new ArrayList<>(threadCount);
+        List<Thread> threadList = new ArrayList<>(threadsCount);
 
-        for (int i = 0; i < threadCount; i++) {
-            if (i + 1 == threadCount) {
+        for (int i = 0; i < threadsCount; i++) {
+            if (i + 1 == threadsCount) {
                 threadList.add(new CountThread(i + 1, i * sizeOfThread, arraySize - 1, array));
             } else {
                 threadList.add(new CountThread(i + 1, i * sizeOfThread, (i + 1) * sizeOfThread - 1, array));
--- day03/src/ex03/Program.java
+++ day03/src/ex03/Program.java
@@ -1,7 +1,49 @@
 package ex03;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Program {
     public static void main(String[] args) {
+//        if (args.length != 1) {
+//            System.err.println("Should be an argument: --threadsCount=_");
+//            System.exit(-1);
+//        }
+//
+//        int threadsCount = 0;
+//
+//        if (args[0].startsWith("--threadsCount")) {
+//            threadsCount = Integer.parseInt(args[0].substring(14));
+//        }
 
+        int threadsCount = 3;
+        Queue<String> urls = new LinkedList<>();
+
+        try {
+            BufferedReader buffer = new BufferedReader(new FileReader("ex03/files_urls.txt"));
+            String line;
+
+            while ((line = buffer.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    urls.add(line);
+                }
+            }
+
+            buffer.close();
+
+            DownloadThread thread = new DownloadThread();
+            thread.setUrls(urls);
+
+            for (int i = 0; i < threadsCount; i++) {
+                thread.start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
