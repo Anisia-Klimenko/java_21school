479b1e39905a16a6b2174d2d3c63f3fa552fceae
Attack Cristina <acristin@at-j6.msk.21-school.ru>
2022-07-04 16:03:40 +0300

    day05/ex01 -> init

---
+++ day05/ex01/Chat/src/main/java/edu/school21/chat/app/Program.java
@@ -0,0 +1,11 @@
+package edu.school21.chat.app;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.print("Enter a message ID\n -> ");
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
