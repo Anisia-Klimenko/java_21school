ee1044cf97ce94d9d8d03229eb2dc053c03cdc6d
Attack Cristina <acristin@at-o7.msk.21-school.ru>
2022-07-28 18:01:43 +0300

    day09/ex00 -> add jcommander

--- day09/ex00/Chat/SocketServer/src/main/java/edu/school21/sockets/app/Main.java
+++ day09/ex00/Chat/SocketServer/src/main/java/edu/school21/sockets/app/Main.java
@@ -1,7 +1,22 @@
 package edu.school21.sockets.app;
 
-public class Main {
-    public static void main(String[] args) {
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import edu.school21.sockets.server.Server;
 
+@Parameters(separators = "=")
+public class Main {
+    @Parameter(names = {"--port", "-p"})
+    int port;
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        JCommander.newBuilder().addObject(main).build().parse(args);
+        main.run();
+    }
+
+    public void run() {
+        Server server = new Server(port);
     }
 }
--- day09/ex00/Chat/SocketServer/src/main/java/edu/school21/sockets/server/Server.java
+++ day09/ex00/Chat/SocketServer/src/main/java/edu/school21/sockets/server/Server.java
@@ -1,4 +1,11 @@
 package edu.school21.sockets.server;
 
 public class Server {
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+
 }
